c0f0b69f1e52886a11eb0ffc0a8ef72dddd525fd
Faeez Shaikh <faeezshaikh@Faeezs-MacBook-Pro.local>
2023-12-22 21:00:06 -0600

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
   email: string | undefined;
   public appPages = [
     { title: 'Capability Map', url: '/capabilities', icon: 'map' }, 
-    { title: 'Value Map', url: '/valuemap', icon: 'grid' }, 
+    { title: 'Value Stream Map', url: '/valuemap', icon: 'grid' }, 
     { title: 'Information Map', url: '/infomap', icon: 'newspaper' }, 
     // { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
     // { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
